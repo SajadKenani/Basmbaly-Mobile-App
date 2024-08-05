ee8366b62e9016d09b75e7a7f36df988e5651067
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2024-08-05 22:25:42 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.qafmanager.app',
-  appName: 'بصملي: تطبيق المدير',
-  webDir: 'build'
-};
-
-export default config;
